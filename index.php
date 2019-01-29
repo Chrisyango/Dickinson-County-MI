34a078f8fa19d43ad90ae313047f9971816b5a6f
Chris Yang <cyang287@yahoo.com>
2019-01-29 14:01:30 -0500

    mark up for html finished

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<title>SiteName</title>
+	<meta name="description" content="">
+	<meta name="robots" content="index, follow">
+
+	<link rel="stylesheet" href="_assets_/plugins/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="_assets_/fonts/font-awesome/css/font-awesome.min.css">
+	<!-- <link rel="stylesheet" href="_assets_/css/animate.min.css"> -->
+	<link rel="stylesheet" href="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" href="_assets_/css/layout.css">
+
+	<link rel="shortcut icon" href="_assets_/images/favicon.ico">
+	<link rel="apple-touch-icon" href="_assets_/images/touch-icon-iphone.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="_assets_/images/touch-icon-ipad.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="_assets_/images/touch-icon-iphone4.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="_assets_/images/touch-icon-ipad2.png">
+
+	<!--[if !IE]><!-->
+	<link rel="stylesheet" href="_assets_/plugins/add-to-homescreen/style/addtohomescreen.css">
+	<script src="_assets_/plugins/add-to-homescreen/src/addtohomescreen.min.js"></script>
+	<script>addToHomescreen();</script>
+	<!--<![endif]-->
+
+	<!-- Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body id="homepage">
+
+<nav class="nav navbar navbar-styles">
+	<a id="logo" href="./"><img src="./_assets_/images/logo.png"></a>
+	<ul class="nav-links">
+		<li class="nav-link"><a href="./">Home</a></li>
+		<li class="nav-link"><a href="./">Government</a></li>
+		<li class="nav-link"><a href="./">Residents</a></li>
+		<li class="nav-link"><a href="./">Businesses</a></li>
+		<li class="nav-link"><a href="./">Visitors</a></li>
+		<li class="nav-link"><a href="./">How Do I...</a></li>
+	</ul><!-- .nav-links -->
+	<div id="search">
+		<form class="search-form" method="get" action="search.php">
+			<label class="fa fa-search" for="search-input"></label>
+			<input name="q" class="form-control search-input" placeholder="Enter search terms..." type="search" id="search-input">
+		</form>
+	</div><!-- /#search -->
+</nav><!-- .nav.navbar -->
+
+
+<section id="hero">
+	<div class="hero-image text-center">
+
+	</div>
+</section><!-- #hero -->
+
+<section id="quick-links">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-">
+			</div>
+		</div>
+	</div>
+</section><!-- #quick-links -->
+
+<section id="welcome" class="text-center">
+	<p>Welcome to <span>Dickinson County</span></p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</section><!-- #welcome -->
+
+<section id="live-work-play">
+	<div class="live">
+		<h2>Live</h2>
+	</div>
+	<div class="work">
+		<h2>Work</h2>
+	</div>
+	<div class="play">
+		<h2>Play</h2>
+	</div>
+</section><!-- #live-work-play -->
+
+<section id="info">
+	<div class="container">
+		<div class="">
+			<img src="./_assets_/images/users.png">
+			<p>27,472<br>population</p>
+		</div>
+		<div class="">
+			<img src="./_assets_/images/rulers-combined.png">
+			<p>220,000<br>acres of public land</p>
+		</div>
+		<div class="">
+			<img src="./_assets_/images/tint.png">
+			<p>6,400<br>acres of lakes</p>
+		</div>
+		<div class="">
+			<img src="./_assets_/images/leaf.png">
+			<p>30+<br>parks & campgrounds</p>
+		</div>
+	</div>
+</section><!-- #info -->
+
+<section id="footer-links">
+	<div id="county-departments">
+		<h4>County Departments</h4>
+		<ul>
+			<li><a href="./">Board of Commissioners</a></li>
+			<li><a href="./">Register of Deeds</a></li>
+			<li><a href="./"></a>County Controller</li>
+			<li><a href="./">Friend of the Court</a></li>
+			<li><a href="./">County Clerk</a></li>
+			<li><a href="./">County Treasurer</a></li>
+			<li><a href="./">Courts</a></li>
+		</ul>
+	</div>
+	<div id="county-departments">
+		<h4>County Departments</h4>
+		<ul>
+			<li><a href="./">Drain Commissioner</a></li>
+			<li><a href="./">Equalization</a></li>
+			<li><a href="./">Sheriff's Department</a></li>
+			<li><a href="./">Parks</a></li>
+			<li><a href="./">Prosecuting Attorney</a></li>
+			<li><a href="./">Ford Airport</a></li>
+		</ul>
+	</div>
+	<div id="quick-links">
+		<h4>Quicklinks</h4>
+		<ul>
+			<li><a href="./">Documents</a></li>
+			<li><a href="./">Elections</a></li>
+			<li><a href="./">Calendar</a></li>
+			<li><a href="./">Parks & Rec</a></li>
+			<li><a href="./">Contact Information</a></li>
+		</ul>
+	</div>
+	<div id="contact-us">
+		<div id="contact-us">
+		<h4>Contact Us</h4>
+		<p>705 S. Stephenson Ave.<br>Iron Mountain MI 49801</p>
+		</div>
+		<div id="mailing-address">
+		<h4>Mailing Address</h4>
+		<p>PO Box 609<br>Iron Mountain MI 49801</p>
+		</div>
+	</div>
+</section><!-- #footer-links -->
+
+<footer>
+	<p>powered by <span>revize.</span> | <a href="./">login</a></p>
+</footer>
+
+	<div class="alert alert-dismissible floating-alert fade in os-animation" data-os-animation="fadeInUpBig" data-os-animation-delay="2s" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		<h1>Alert Title 30 characters Max</h1>
+		<p>Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120</p>
+		<p><a href="#" class="btn">Read More</a></p>
+	</div><!-- /.alert -->
+
+</div><!-- /.main-wrap -->
+
+
+<!-- Share widget make into an include file -->
+<button type="button" class="share-btn floating-share-btn" data-toggle="modal" data-target="#shareModal">
+	<i class="fa fa-share-alt"></i>
+</button>
+
+<div class="modal fade" id="shareModal" tabindex="-1" role="dialog" aria-labelledby="shareModal">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Share this page</h4>
+			</div>
+			<div class="modal-body">
+				<div class="copylink">
+					<p>Copy and paste this code into your website.</p>
+					<pre>&lt;a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">Your Link Name&lt;/a&gt;</pre>
+				</div><!-- /.copylink -->
+				<div class="share-btns">
+					<p>Share this page on your favorite Social network</p>
+					<div class="row">
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-facebook" onclick="return !window.open(this.href, 'facebook ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-facebook"></i> Facebook
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.twitter.com/home?status=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-twitter" onclick="return !window.open(this.href, 'twitter ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-twitter"></i> Twitter
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.plus.google.com/share?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-google" onclick="return !window.open(this.href, 'google ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-google-plus"></i> Google Plus
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.reddit.com/submit?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-reddit" onclick="return !window.open(this.href, 'redit ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-reddit"></i> Reddit
+							</a>
+						</div>
+					</div><!-- /.row -->
+				</div><!-- /.share-btns -->
+				<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">Close</button>
+			</div><!-- /.modal-body -->
+		</div>
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- Share widget make into an include file -->
+
+
+<script src="_assets_/js/jquery.min.js"></script>
+<script src="_assets_/plugins/modernizr/modernizr.custom.js"></script>
+<script src="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.min.js"></script>
+<script src="_assets_/plugins/jquery.bxslider/jquery.bxslider.min.js"></script>
+<script src="_assets_/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="_assets_/plugins/sticky/jquery.sticky.js"></script>
+<script src="_assets_/plugins/matchHeight/dist/jquery.matchHeight-min.js"></script>
+<script src="_assets_/plugins/waypoints/waypoints.min.js"></script>
+<script src="_assets_/js/scripts.js"></script>
+
+</body>
+</html>
