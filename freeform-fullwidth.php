4c91fab671cee6ff55dba0d2778b335ac5708481
Chris Yang <cyang287@yahoo.com>
2019-02-05 16:11:39 -0500

    copied freeform.php into freeform-fullwidth.php

--- freeform-fullwidth.php
+++ freeform-fullwidth.php
@@ -36,10 +36,249 @@
 
 		</head>
 		<body id="freeform" class="fullwidth">
-		
-			<div class="main-wrap">	
+
+		<div class="main-wrap">	
 				<!-- Content Here -->
-			</div><!-- /.main-wrap -->  
+				<!-- NOTE: Test Flyout with a tags and spans just like the main nav. Cannot assume all items are links. -->
+				<header class="clearfix">
+					<nav class="clearfix">
+						<a href="./" class="pull-left" id="logo"><img src="_assets_/images/logo.png" alt="navigation logo"></a>
+						<div id="nav-toggle" class="fa fa-bars pull-right"></div>
+						<div id="search-toggle" class="fa fa-search pull-right"></div>
+						<ul id="nav" class="pull-right">
+						<li><a href="./">Home</a></li>
+							<li>
+								<a href="./">Government</a>
+								<ul>
+									<li>
+										<a href="./">Section Header</a>
+										<ul>
+											<li><a href="./">Euismod Cras Vestibulum</a></li>
+											<li><a href="./">Mollis Vulputate Mattis</a></li>
+											<li><a href="./">Fusce Mollis Commodo</a></li>
+											<li><a href="./">Inceptos Justo Risus Tortor</a></li>
+											<li><a href="./">Euismod Tellus Euismod</a></li>
+											<li><a href="./">Cras Vestibulum Mollis</a></li>
+											<li><a href="./">Vulputate Mattis Fusce</a></li>
+											<li><a href="./">Mollis Commodo Inceptos</a></li>
+										</ul>
+									</li>
+									<li>
+										<a href="./">Section Header</a>
+										<ul>
+											<li><a href="./">Euismod Cras Vestibulum</a></li>
+											<li><a href="./">Mollis Vulputate Mattis</a></li>
+											<li><a href="./">Fusce Mollis Commodo</a></li>
+										</ul>
+									</li>
+									<li>
+										<a href="./">Section Header</a>
+										<ul>
+											<li><a href="./">Inceptos Justo Risus Tortor</a></li>
+											<li><a href="./">Euismod Tellus Euismod</a></li>
+										</ul>
+									</li>
+									<li>
+										<a href="./">Section Header</a>
+										<ul>
+											<li><a href="./">Inceptos Justo Risus Tortor</a></li>
+											<li><a href="./">Euismod Tellus Euismod</a></li>
+											<li><a href="./">Cras Vestibulum Mollis</a></li>
+										</ul>
+									</li>
+									<li><a href="./">Section Header</a></li>
+									<li>
+										<a href="./">Section Header</a>
+										<ul>
+											<li><a href="./">Vulputate Mattis Fusce</a></li>
+											<li><a href="./">Mollis Commodo Inceptos</a></li>
+										</ul>
+									</li>
+									<li>
+										<a href="./">Section Header</a>
+										<ul>
+											<li><a href="./">Euismod Cras Vestibulum</a></li>
+											<li><a href="./">Mollis Vulputate Mattis</a></li>
+											<li><a href="./">Fusce Mollis Commodo</a></li>
+											<li><a href="./">Inceptos Justo Risus Tortor</a></li>
+											<li><a href="./">Euismod Tellus Euismod</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li><a href="./">Residents</a></li>
+							<li><a href="./">Businesses</a></li>
+							<li><a href="./">Visitors</a></li>
+							<li><a href="./">How Do I...</a></li>
+						</ul>
+					</nav>
+					<div id="search">
+						<form class="search-form" method="get" action="search.php">
+							<label class="fa fa-search fa-lg" for="search-input"></label>
+							<input name="q" class="form-control search-input" placeholder="Enter keywords..." type="search" id="search-input">
+							<button type="submit">Go</button>
+						</form>
+					</div><!-- /#search -->
+				</header>
+
+				<main>
+				<section id="slider">
+					<ul class="bxslider">
+						<li style="background:url('./_assets_/images/hero.jpg') center no-repeat;background-size:cover;">
+						</li>
+					</ul><!--/.bxslider-->
+					<div class="container text-center">
+						<h1>
+							interior page title
+						</h1>
+						<section id="breadcrumbs">
+							<p>
+								<a href="./">
+									home
+								</a>
+								/
+								<a href="./">
+									parent page
+								</a>
+								/
+								current page
+							</p>
+						</section><!-- /#breadcrumbs -->
+					</div>
+					<img src="./_assets_/images/hero-whitespace.png" alt="Whitespace">
+				</section><!--/#slider-->
+
+				<div class="container">
+					<div class="row">
+
+						<div class="col-md-9">
+							<article class="entry">
+								<div class="post">
+									<p>
+										Lorem ipsum dolor sit amet consectetur, adipisicing elit. A magnam in voluptatem, qui illum non vel nihil accusamus nobis beatae, impedit debitis itaque id cum vero sapiente laborum reiciendis consequuntur. Lorem ipsum dolor sit amet consectetur, adipisicing elit. A magnam in voluptatem, qui illum non vel nihil accusamus nobis beatae, impedit debitis itaque id cum vero sapiente laborum reiciendis consequuntur.
+									</p>
+									<h2>header two (h2)</h2>
+									<p>
+										Lorem ipsum dolor sit amet consectetur, adipisicing elit. A magnam in voluptatem, qui illum non vel nihil accusamus nobis beatae, impedit debitis itaque id cum vero sapiente laborum reiciendis consequuntur.
+										Lorem ipsum dolor sit amet consectetur, adipisicing elit. A magnam in voluptatem, qui illum non vel nihil accusamus nobis beatae, impedit debitis itaque id cum vero sapiente laborum reiciendis consequuntur.
+										Lorem ipsum dolor sit amet consectetur, adipisicing elit. A magnam in voluptatem, qui illum non vel nihil accusamus nobis beatae, impedit debitis itaque id cum vero sapiente laborum reiciendis consequuntur.
+									</p>
+								</div>
+							</article><!-- /.entry -->
+						</div><!-- /.col-md-9 -->
+					</div><!-- /.row -->
+				</div><!-- /.container -->
+
+				<section id="footer-links" style="background:url(_assets_/images/footer-links.jpg) center no-repeat;background-size:cover">
+					<img src="./_assets_/images/footer-links-whitespace.png" alt="Whitespace">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-3">
+								<h5>County Departments</h5>
+								<ul>
+									<li><a href="./">Board of Commissioners</a></li>
+									<li><a href="./">Register of Deeds</a></li>
+									<li><a href="./">County Controller</a></li>
+									<li><a href="./">Friend of the Court</a></li>
+									<li><a href="./">County Clerk</a></li>
+									<li><a href="./">County Treasurer</a></li>
+									<li><a href="./">Courts</a></li>
+								</ul>
+							</div>
+							<div class="col-md-3">
+								<h5>County Departments</h5>
+								<ul>
+									<li><a href="./">Drain Commissioner</a></li>
+									<li><a href="./">Equalization</a></li>
+									<li><a href="./">Sheriff's Department</a></li>
+									<li><a href="./">Parks</a></li>
+									<li><a href="./">Prosecuting Attorney</a></li>
+									<li><a href="./">Ford Airport</a></li>
+								</ul>
+							</div>
+							<div class="col-md-3">
+								<h5>Quicklinks</h5>
+								<ul>
+									<li><a href="./">Documents</a></li>
+									<li><a href="./">Elections</a></li>
+									<li><a href="./">Calendar</a></li>
+									<li><a href="./">Parks & Rec</a></li>
+									<li><a href="./">Contact Information</a></li>
+								</ul>
+							</div>
+							<div id="contact" class="col-md-3">
+								<div id="contact-us">
+									<h5>Contact Us</h5>
+									<p>705 S. Stephenson Ave.<br>Iron Mountain MI 49801</p>
+								</div>
+								<div id="mailing-address">
+									<h5>Mailing Address</h5>
+									<p>PO Box 609<br>Iron Mountain MI 49801</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section><!-- #footer-links -->
+				</main>
+
+				<footer class="text-center">
+					<p>powered by <a href="https://www.revize.com/">revize.</a> | <a class="login" href="./">login</a></p>
+				</footer>
+				</div><!-- /.main-wrap -->  
+
+				<!-- Share widget make into an include file -->
+				<button type="button" class="share-btn floating-share-btn" data-toggle="modal" data-target="#shareModal">
+					<i class="fa fa-share-alt"></i>
+					<span>SHARE</span>
+				</button>
+
+				<div class="modal fade" id="shareModal" tabindex="-1" role="dialog" aria-labelledby="shareModal">
+					<div class="modal-dialog modal-lg" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+								<h4 class="modal-title" id="myModalLabel">Share this page</h4>
+							</div>
+							<div class="modal-body">
+								<div class="copylink">
+									<p>Copy and paste this code into your website.</p>
+									<pre>&lt;a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">Your Link Name&lt;/a&gt;</pre>
+								</div><!-- /.copylink -->
+								<div class="share-btns">
+									<p>Share this page on your favorite Social network</p>
+									<div class="row">
+										<div class="col-sm-3 col-xs-12">
+											<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-facebook" onclick="return !window.open(this.href, 'facebook ', 'width=500,height=500')"
+											target="_blank">
+												<i class="fa fa-facebook"></i> Facebook
+											</a>
+										</div>
+										<div class="col-sm-3 col-xs-12">
+											<a href="https://www.twitter.com/home?status=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-twitter" onclick="return !window.open(this.href, 'twitter ', 'width=500,height=500')"
+											target="_blank">
+												<i class="fa fa-twitter"></i> Twitter
+											</a>
+										</div>
+										<div class="col-sm-3 col-xs-12">
+											<a href="https://www.plus.google.com/share?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-google" onclick="return !window.open(this.href, 'google ', 'width=500,height=500')"
+											target="_blank">
+												<i class="fa fa-google-plus"></i> Google Plus
+											</a>
+										</div>
+										<div class="col-sm-3 col-xs-12">
+											<a href="https://www.reddit.com/submit?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-reddit" onclick="return !window.open(this.href, 'redit ', 'width=500,height=500')"
+											target="_blank">
+												<i class="fa fa-reddit"></i> Reddit
+											</a>
+										</div>
+									</div><!-- /.row -->
+								</div><!-- /.share-btns -->
+								<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">Close</button>
+							</div><!-- /.modal-body -->
+						</div>
+					</div><!-- /.modal-dialog -->
+				</div><!-- /.modal -->
+				<!-- Share widget make into an include file -->
 
 <script src="_assets_/js/jquery.min.js"></script>
 <script src="_assets_/plugins/modernizr/modernizr.custom.js"></script>
